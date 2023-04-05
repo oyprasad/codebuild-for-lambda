3337ddec9d4abc1c15bfbfb491136d985d4a9229
oyprasad <60344984+oyprasad@users.noreply.github.com>
2023-04-05 19:05:25 +0530

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ export const handler = async(event) => {
     // TODO implement
     const response = {
         statusCode: 200,
-        body: JSON.stringify('Hello from Lambda!-updated-check'),
+        body: JSON.stringify('Hello from Lambda!-updated-check-test'),
     };
     return response;
 };
